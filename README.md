65321615be9e7449facfdf1d43fac12aba5566c0
pungow <holamundohw@gmail.com>
2021-09-05 18:17:03 -0500

    Complete Taller1 & Taller2

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # curso-practico-JS
+
+..
+
+## Taller #1: figuras geometricas
+
+-Primer paso: definir las formulas
+-Segundo paso: implementar las formulas en JS
+-Tercer paso: crear funciones
+-Cuarto paso: integrar JS con HTML
+
+## Taller #2: Precios y descuentos
+
+-Primer paso: definir las formulas
+-Segundo paso: implementar las formulas en JS
+-Tercer paso: crear funciones
+-Cuarto paso: integrar JS con HTML
---
+++ Taller2/precios.js
@@ -0,0 +1,20 @@
+
+function calcularPrecioConDescuento(precio, descuento) {
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+
+    return precioConDescuento;
+}
+
+function onClickPriceDiscount() {
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue= inputDiscount.value;
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+    const resultPrice = document.getElementById("ResultPrice");
+    resultPrice.innerHTML = "El precio con descuento es: $" + precioConDescuento;
+}
